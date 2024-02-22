2a7a199cf89d407c8ceec3f78605c167ca8afc17
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2024-02-22 15:52:27 +0000

    1b working but no break condition

--- COMP3002 CW1/COMP3002 CW1/COMP3002_CW1.py
+++ COMP3002 CW1/COMP3002 CW1/COMP3002_CW1.py
@@ -16,15 +16,18 @@ else:
 
 # Q1b Generate all possible permutations of an array
 
-def all_permutations_1b(arr):
+def all_permutations_1b(arr, idx): # Need to make a proper break condition - currently running forever
     if len(arr) <= 1:
         return arr
-    arr[0], arr[1] = arr[1], arr[0]
+    if idx >= len(arr) - 1:
+        idx = 0
     print(arr)
-    return all_permutations_1b(arr[1:])
+    arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+    
+    return all_permutations_1b(arr, idx + 1)
 
 arr = [1, 2, 3]
-# all_permutations_1b(arr)
+all_permutations_1b(arr, 0)
 
 # Q1c LCM and GCD
 def lcm_gcd_1c(nums):
@@ -33,12 +36,11 @@ def lcm_gcd_1c(nums):
     elif nums[0] == 0:
         return lcm_gcd_1c(nums[1:])
     else:
-
         nums[1] = nums[1] % nums[0]
         nums[0], nums[1] = nums[1], nums[0]
         return lcm_gcd_1c(nums)
 
-nums = [46, 64, 578]
+nums = [46, 64, 578, 468, 18, 32]
 print("GCD = " + str(lcm_gcd_1c(nums)))
 # Q1d Decimal to binary
 
